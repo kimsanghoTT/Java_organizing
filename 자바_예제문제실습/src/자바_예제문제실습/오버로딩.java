414ccf4009efc4b09d4fbc099f085072622e9136
kimsanghoTT <165999006+kimsanghoTT@users.noreply.github.com>
2024-05-09 09:23:20 +0900

    직렬화, 역직렬화

--- 자바_예제문제실습/src/자바_예제문제실습/오버로딩.java
+++ 자바_예제문제실습/src/자바_예제문제실습/오버로딩.java
@@ -4,7 +4,7 @@ public class 오버로딩 {
 
 //매개변수 갯수에 따라 다른 생성자가 됨. -> 같은 이름도 여러개 가능
 	
-	public void test() {}
+	public void test(double p) {}
 	public void test(String str, String a) {}
 	public void test(String s) {}
 
@@ -13,4 +13,34 @@ public class 오버로딩 {
 	
 	public void test(char a, char ch) {}
 	void test(char ch) {}
+	
+	public void test(boolean a, String x, int y, short z, char k, byte b
+			, long p, float f, double d) {}
+	public void test(boolean b) {}
+	public void test(short a, long l) {}
+	public void test(short b) {}
+	
+	public void test(int a, String b) {}
+	public void test(int a, String b, double d) {}
+	
+	public void test(String c, int b) {}
+	public void test(String c, int b, String a) {}
+	
+	public void test(boolean a, boolean b) {}
+	public void test(boolean a, boolean b, boolean c) {}
+	
+	public String test() {
+		return "test";
+		
+	}
+	public int test(short i, short o) {
+		return 0;
+	}
+	public double test(String a, String b, String c) {
+		return 0.1;
+	}
+	
+	
+	
+	
 }
--- 자바테스트_정리하기/src/com/kh/test/main/ㅅㅅㅅ.java
+++ 자바테스트_정리하기/src/com/kh/test/main/ㅅㅅㅅ.java
@@ -1,7 +1,12 @@
 package com.kh.test.main;
 
-import java.util.Scanner;
+
 
 public class ㅅㅅㅅ {
 
+
+	public static void main(String[] args) {
+		int[][]count={{3,4,6,},{7,4,5}};
+		System.out.println(count.length);
+	}
 }
